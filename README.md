eb164fd7a7ae640cbbde3f1062342fc641a86ce2
Akshaya <132545615+AKSHAYAPALCHAMY-P@users.noreply.github.com>
2026-09-14 15:17:22 +0530

    Components (#2)
    * ADDEDL: Tailwind setup
    
    * ADDED: Final Logics
    
    * CHANGED: R#
    
    ---------
    
    Co-authored-by: Akshaya <akshaya@astravor.com>

--- README.md
+++ README.md
@@ -57,3 +57,17 @@ Angular CLI does not come with an end-to-end testing framework by default. You c
 ## Additional Resources
 
 For more information on using the Angular CLI, including detailed command references, visit the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+
+## Create new project
+
+ng new applicationName
+
+## create new component
+
+```ng g c ComponentName
+```
+
+## Run
+```
+ng serve
+```
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from './menu/menu';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
+  //template: '<h1 class="text-3xl font-bold underline">Hello, Angular!</h1>',
 })
 export class App {
   protected readonly title = signal('app');
